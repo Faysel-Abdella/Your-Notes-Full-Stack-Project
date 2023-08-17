dcc7399cf874a874a2e734a0443a45cf78897760
DESKTOP-GHRRVG9\pc <fayselcode@gmail.com>
2023-08-18 00:42:45 +0100

    hover effect is added

--- src/assets/wrappers/tasksWrapper.js
+++ src/assets/wrappers/tasksWrapper.js
@@ -1,7 +1,75 @@
 import { styled } from "styled-components";
 
 const Wrapper = styled.section`
+  height: 100%;
+  width: 100%;
   border-radius: 5px;
+  position: relative;
+  background: var(--secondary-background);
+  /* padding: 1.8rem 2.2rem; */
+
+  z-index: 999;
+  .tasks-container {
+  }
+
+  .task-container {
+    display: flex;
+    align-items: center;
+    border-bottom: 1.5px solid #e3e4f1;
+    padding: 20px 24px 15px 24px;
+  }
+
+  .check-container {
+    border: 1px solid var(--main-color);
+    border-radius: 50%;
+    width: 24px;
+    height: 24px;
+    /* display: flex;
+    align-items: center;
+    justify-content: center; */
+  }
+  .check-icon {
+    display: none;
+  }
+  .check-container:hover .check-icon {
+    cursor: pointer;
+    display: block;
+    font-size: 18px;
+    background: var(--main-color);
+    padding: 1px;
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+    color: var(--white-color);
+  }
+
+  .task-text {
+    font-size: var(--medium-text);
+    color: var(--task-color);
+    margin-left: 24px;
+  }
+  .complete {
+    .check-icon {
+      /* transform: translate(-50%, -50%); */
+      display: block;
+      font-size: 18px;
+      background: var(--main-color);
+      padding: 1px;
+      border-radius: 50%;
+      width: 100%;
+      height: 100%;
+      color: var(--white-color);
+    }
+
+    .task-text {
+      color: var(--complete-task-color);
+      text-decoration-line: line-through;
+    }
+  }
+
+  .cancel {
+    margin-left: auto;
+  }
 `;
 
 export default Wrapper;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,13 +1,37 @@
+import cancel from "../assets/images/cancle.svg";
+
+import { BsCheckLg } from "react-icons/bs";
+
 import Wrapper from "../assets/wrappers/tasksWrapper";
-import Task from "../components/Task";
+
+const tasks = [
+  { title: "Do the first task", state: "not-complete" },
+  { title: "Do the first task", state: "not-complete" },
+  { title: "Do the first task", state: "not-complete" },
+  { title: "Do the first task", state: "not-complete" },
+];
 
 const Tasks = () => {
   return (
     <Wrapper>
-      <Task />
-      <Task />
-      <Task />
-      <Task />
+      <div className="tasks-container">
+        {tasks.map((task, index) => (
+          <div
+            className={`task-container ${true ? "com" : ""}`}
+            key={Math.random()}
+          >
+            <span className="check-container">
+              <BsCheckLg className="check-icon" />
+            </span>
+
+            <p className="task-text">do one task</p>
+
+            <button className="cancel">
+              <img src={cancel} alt="" />
+            </button>
+          </div>
+        ))}
+      </div>
     </Wrapper>
   );
 };
